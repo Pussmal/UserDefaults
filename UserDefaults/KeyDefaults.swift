027af006e0bce6ab98b02e5231eec084a436434b
Motorin Aleksey <aleksey.motorin2015@yandex.ru>
2022-07-11 21:54:57 +0300

    save and load entered adress

---
+++ UserDefaults/KeyDefaults.swift
@@ -0,0 +1,12 @@
+//
+//  KeyDefaults.swift
+//  UserDefaults
+//
+//  Created by Алексей Моторин on 11.07.2022.
+//
+
+import Foundation
+
+enum KeyDefaults: String {
+    case city, stret, house, build, flat
+}
